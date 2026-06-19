4f487600e6c2582305a39213f4439270313e7e47
SamuBury007 <samybury07@gmail.com>
2026-06-19 16:08:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 pip install flask playwright requests
-python -m playwright install chromium
-python -m playwright install-deps chromium
+python -m playwright install --with-deps chromium
